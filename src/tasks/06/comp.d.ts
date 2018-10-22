7e3bbb52e6f1f53d5594fbfc3b75da08902ed886
Anders Olav Candasamy <anders.can1@gmail.com>
2018-10-22 23:00:36 +0200

    more solutions

--- src/tasks/06/comp.d.ts
+++ src/tasks/06/comp.d.ts
@@ -1,1 +1,9 @@
 // Type comp.jsx here
+interface SquashScoreCompProps {
+  name: string;
+  score: number;
+}
+
+export declare function SquashScoreComp(): React.Component<
+  SquashScoreCompProps
+>;
